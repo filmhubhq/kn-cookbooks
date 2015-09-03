18637c4fc7892a2beb69f5f8d27021901752a4d2
David Newkerk <keysplayer@gmail.com>
2015-09-03 00:38:41 -0700

    Try a different approach for restarting the sidekiq_workers service.

--- opsworks-sidekiq/recipes/deploy.rb
+++ opsworks-sidekiq/recipes/deploy.rb
@@ -2,11 +2,8 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Running Sidekiq deploy for #{application}")
 
-  # The restart action starts the service if it is stopped or
-  # restarts it if it is already running.
-  service 'sidekiq_workers' do
-    provider Chef::Provider::Service::Upstart
-    supports :status => true, :restart => true, :reload => true
-    action :restart
+  bash 'restart_sidekiq' do
+    code 'echo noop'
+    notifies :restart, 'service[sidekiq_workers]'
   end
 end
