7bcbd29ce7a9bdd4e7ad333ad89364a6b6789047
David Newkerk <keysplayer@gmail.com>
2015-09-03 00:51:08 -0700

    Include the configure recipe so the service will be accessible.

--- opsworks-sidekiq/recipes/deploy.rb
+++ opsworks-sidekiq/recipes/deploy.rb
@@ -2,6 +2,9 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Running Sidekiq deploy for #{application}")
 
+  # Include the configure recipe so the service will be accessible.
+  include_recipe 'opsworks-sidekiq::configure'
+
   bash 'restart_sidekiq' do
     code 'echo noop'
     notifies :restart, 'service[sidekiq_workers]'
