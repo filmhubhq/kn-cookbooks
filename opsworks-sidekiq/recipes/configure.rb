ddd608e5dea58a9d5bbbc935c314e0282c3bc6d4
David Newkerk <keysplayer@gmail.com>
2015-09-02 22:30:32 -0700

    Make the number of workers to launch configurable.

--- opsworks-sidekiq/recipes/configure.rb
+++ opsworks-sidekiq/recipes/configure.rb
@@ -2,11 +2,17 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Setting up Sidekiq workers for #{application}")
 
+  config = deploy[:sidekiq] if !deploy[:sidekiq].nil?
+  number_of_workers = config[:number_of_workers] || 1
+
   template '/etc/init/sidekiq_workers.conf' do
     source 'sidekiq_workers.conf.erb'
     mode   '0644'
     owner  'root'
     group  'root'
+    variables(
+      number_of_workers: number_of_workers
+    )
   end
 
   template '/etc/init/sidekiq.conf' do
