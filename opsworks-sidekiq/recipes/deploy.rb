ca56aaddd5c6a98efa9c08f3992b0090d72db053
David Newkerk <keysplayer@gmail.com>
2015-09-02 21:17:22 -0700

    Ensure the deploy restart action uses upstart, was trying to find the workers service in /etc/init.d (SystemV).

--- opsworks-sidekiq/recipes/deploy.rb
+++ opsworks-sidekiq/recipes/deploy.rb
@@ -3,6 +3,7 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("Running Sidekiq deploy for #{application}")
 
   service 'workers' do
+    provider Chef::Provider::Service::Upstart
     action :restart
   end
 end
